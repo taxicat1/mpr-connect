47a5c5d9f1400c0f7a65e1812ac7881e0a99c21a
taxicat1 <32942550+taxicat1@users.noreply.github.com>
2026-06-26 20:04:39 -0400

    Static var to global

--- arm9/source/game_code_support.c
+++ arm9/source/game_code_support.c
@@ -3,35 +3,35 @@
 #include "game_code_support.h"
 #include "pkmn_game_codes.h"
 
+static const u32 sSupportedGameCodes[] = {
+	GAME_CODE_D_EN,
+	GAME_CODE_D_JA,
+	GAME_CODE_D_DE,
+	GAME_CODE_D_ES,
+	GAME_CODE_D_FR,
+	GAME_CODE_D_IT,
+	
+	GAME_CODE_P_EN,
+	GAME_CODE_P_JA,
+	GAME_CODE_P_DE,
+	GAME_CODE_P_ES,
+	GAME_CODE_P_FR,
+	GAME_CODE_P_IT,
+	
+	GAME_CODE_PT_EN,
+	GAME_CODE_PT_JA,
+	GAME_CODE_PT_DE,
+	GAME_CODE_PT_ES,
+	GAME_CODE_PT_FR,
+	GAME_CODE_PT_IT,
+	
+	GAME_CODE_OTHER  // EOL
+};
+
 
 bool GameCode_IsSupported(u32 game_code) {
-	static const u32 supported_game_codes[] = {
-		GAME_CODE_D_EN,
-		GAME_CODE_D_JA,
-		GAME_CODE_D_DE,
-		GAME_CODE_D_ES,
-		GAME_CODE_D_FR,
-		GAME_CODE_D_IT,
-		
-		GAME_CODE_P_EN,
-		GAME_CODE_P_JA,
-		GAME_CODE_P_DE,
-		GAME_CODE_P_ES,
-		GAME_CODE_P_FR,
-		GAME_CODE_P_IT,
-		
-		GAME_CODE_PT_EN,
-		GAME_CODE_PT_JA,
-		GAME_CODE_PT_DE,
-		GAME_CODE_PT_ES,
-		GAME_CODE_PT_FR,
-		GAME_CODE_PT_IT,
-		
-		GAME_CODE_OTHER  // EOL
-	};
-	
-	for (int i = 0; supported_game_codes[i] != GAME_CODE_OTHER; i++) {
-		if (supported_game_codes[i] == game_code) {
+	for (int i = 0; sSupportedGameCodes[i] != GAME_CODE_OTHER; i++) {
+		if (sSupportedGameCodes[i] == game_code) {
 			return TRUE;
 		}
 	}
